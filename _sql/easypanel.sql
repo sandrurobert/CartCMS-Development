70178068cb3e8f1e3ee95a0dc121c83f2e0ecd60
yoX64 <yox64@yahoo.com>
2014-01-20 19:25:22 +0200

    sql basic file

---
+++ _sql/easypanel.sql
@@ -0,0 +1,51 @@
+-- EASYPANEL REQUIRED TABLES
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+02:00";
+
+DROP TABLE IF EXISTS `ep_admin_users`;
+CREATE TABLE IF NOT EXISTS `ep_admin_users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `pass` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_user`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2;                 
+
+-- admin:admin
+INSERT INTO `ep_admin_users` (`id_user`, `user`, `pass`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+DROP TABLE IF EXISTS `ep_admin_settings`;
+CREATE TABLE IF NOT EXISTS `ep_admin_settings` (
+  `id_setting` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `value` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_setting`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4; 
+
+INSERT INTO `ep_admin_settings` (`id_setting`, `name`, `value`) VALUES
+(1, '', 'website_title'),
+(2, '', 'website_logo'),
+(3, '', 'website_copyright');
+
+DROP TABLE IF EXISTS `ep_modules`;
+CREATE TABLE IF NOT EXISTS `ep_modules` (
+  `id_module` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `nickname` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_module`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3; 
+
+INSERT INTO `ep_modules` (`id_module`, `name`, `nickname`) VALUES
+(1, 'Homepage', 'homepage'),
+(2, 'Simple page', 'simple_page');
+
+DROP TABLE IF EXISTS `ep_pages`;
+CREATE TABLE IF NOT EXISTS `ep_pages` (
+  `id_page` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `link_title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `module` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `page_type` int(11) COLLATE utf8_unicode_ci NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_page`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3;
