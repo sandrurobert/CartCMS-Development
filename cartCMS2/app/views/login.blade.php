08b3734ee4d5218be82fbe749e2a2b10475c4e04
Alex Radoi <axxwee@gmail.com>
2014-07-23 13:04:26 +0300

    New login design added

--- cartCMS2/app/views/login.blade.php
+++ cartCMS2/app/views/login.blade.php
@@ -6,35 +6,47 @@
 		{{ HTML::script('js/jquery-1.11.1.min.js'); }}
 		{{ HTML::script('js/custom.js'); }}
 	</head>
-	<body>
-		<div id="container">
-			<div id="logo">
-				<img src="{{asset('img/logo.png')}}" alt="logo" />
-			</div>
-			<div id="box">
-				<div id="boxHeader">
-					<img src="{{asset('img/login.png')}}" alt="login" />
-					<div id="boxHeaderp">
-						<p>Sign In</p>
-					</div>
-					<img src="{{asset('img/x.png')}}" class="floatRight" alt="x" />
-				</div>
-				<div id="infoBox">
-					<img src="{{asset('img/info.png')}}" alt="info" />
-					<p>You’re about to login into CartCMS</p>
-				</div>
-				<div id="loginBox">
-					{{ Form::open(array('route' => 'user.login', 'method' => 'post', 'class' => 'form-login')) }}
-						{{Form::text('username', null, array('placeholder' => 'Username', 'class' => 'username-login', 'id' => 'username', 'title' => 'username'))}}
-						{{Form::password('password', array('id' => 'password', 'class' => 'password-login', 'title' => 'password'))}}
-						{{ Form::submit('Sign In', array('class' => 'submit', 'id' => 'submit-login'))}}
-					{{Form::close()}}
-				</div>
-			</div>
-			<div id="hr"></div>
-			<div id="copyrights">
-				<p>Copyright @ 2014 UserX & Axxwee</p>
-			</div>
-		</div>
+	<body style="background: #293a4b;" >
+
+	    <div id="login-row" class="row" >
+	      <div class="col-md-2 col-md-offset-5" >
+	        <img src="{{asset('img/base/logo.png')}}" class="img-responsive centerize" alt="Logo" />
+	      </div>
+	    </div>  <!-- login-row-->
+			<div class="row">
+	      <div class="col-md-4 col-md-offset-4">
+	        <div class="blueBox">
+	          <div class="boxHeader">
+	            <h4 class="boxHeaderTitle">
+	              <img class="boxHeaderImgLeft" src="{{asset('img/icons/login.png')}}" />
+	                Login Box
+	            </h4>
+	          </div><!-- boxHeader -->
+
+	          <div class="yellowInfo">
+	            <h4 class="infoTitle">
+	              <img class="infoTitleImgLeft" src="{{asset('img/icons/info.png')}}" />
+	                You're about to login into CartCMS!
+	            </h4>
+	          </div><!-- information box -->
+
+	          {{ Form::open(array('route' => 'user.login', 'method' => 'post', 'class' => 'boxContainer')) }}
+	            <div class="boxInputs">
+	              {{Form::text('username', null, array('placeholder' => 'E-mail', 'class' => 'basicInput', 'title' => 'username'))}}
+	            </div>
+	            <div class="boxInputs">
+	              {{Form::password('password', array('id' => 'password', 'class' => 'basicInput', 'title' => 'password', 'placeholder' => '*********'))}}
+	            </div>
+	            <div class="boxButtons">
+	              {{ Form::submit('Sign In', array('class' => 'redBtn width-90', 'id' => 'submit-login'))}}
+	            </div><!--  boxButtons  -->
+	          {{Form::close()}}<!--  boxContainer  -->
+	        </div><!-- blueBox -->
+	        <div class="col-md-6 col-md-offset-6 no-padding">
+	          <a href="#" id="lostPassword">Lost Password?</a>
+	        </div>
+	      </div><!--  col-md-4  -->
+	    </div>
+					
 	</body>
 </html>
